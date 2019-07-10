ade75154658f97fee57cfbbd9ac0acfd4970f79a
Mariana Azevedo <mariana@bsi.ufla.br>
2019-07-10 00:27:57 -0300

    Updates

---
+++ precache-manifest.7c5e0c7d4d46acbfafcf803a122a1485.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef0f0960ea47c1fb2500a4d9af213cd",
+    "url": "/o3smeasures/index.html"
+  },
+  {
+    "revision": "3e70b1c9cf64120438be",
+    "url": "/o3smeasures/static/css/main.2caf3c8f.chunk.css"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/o3smeasures/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "3e70b1c9cf64120438be",
+    "url": "/o3smeasures/static/js/main.6a2ff4a0.chunk.js"
+  },
+  {
+    "revision": "0b4c39a055a7e61f5b40",
+    "url": "/o3smeasures/static/js/runtime~main.220f57d1.js"
+  },
+  {
+    "revision": "e106222880a319d5798967ad5c6aec01",
+    "url": "/o3smeasures/static/media/action.e1062228.png"
+  },
+  {
+    "revision": "a7c00ade39a4651271616d714251c4ee",
+    "url": "/o3smeasures/static/media/graph.a7c00ade.png"
+  },
+  {
+    "revision": "02cf129e07e26a6cc433b1d2c3ad74ff",
+    "url": "/o3smeasures/static/media/measuring.02cf129e.png"
+  }
+]);
